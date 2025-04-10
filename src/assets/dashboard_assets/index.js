bfef4689acbafee583e73b7f06b617bafbf39471
Pallab <pallabkshyp@gmail.com>
2025-04-10 14:38:54 +0530

    SVGs updated

--- src/assets/dashboard_assets/index.js
+++ src/assets/dashboard_assets/index.js
@@ -5,6 +5,9 @@ import hotel from "./hotel.svg";
 import referral from "./referral.svg";
 import reward from "./reward.svg"
 import sellYTP from "./sellYTP.svg";
+import transaction from "./transaction.svg";
+import wallet from "./wallet.svg"
+import withdraw from "./withdraw.svg";
 
 
 
@@ -15,4 +18,7 @@ export {
   referral,
   sellYTP,
   reward,
+  transaction,
+  wallet,
+  withdraw
 };
